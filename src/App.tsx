fe370365bf67ca33c420e562a8e3f7833002b9c3
Firuz M <madaminovfiruz659@gmail.com>
2024-07-07 00:24:36 +0500

    change infinity loader

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,7 @@ import {
 
 import PopupForm from "./components/PopupForm/PopupForm";
 import { useEffect, useState } from "react";
-import { ScrollArea } from "./components/ui/scroll-area";
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { Button } from "./components/ui/button";
 import { useInView } from "react-intersection-observer";
 
 function App() {
@@ -41,12 +39,29 @@ function App() {
       const nextPage = lastPage.next ? allPage.length + 1 : undefined;
       return nextPage;
     },
+    refetchOnWindowFocus: false,
   });
 
   const { ref, inView } = useInView();
 
   const dataList = data?.pages.map((items) =>
-    items.data.map((item) => {
+    items.data.map((item, index) => {
+      console.log(index, "item");
+      console.log(items.data.length, "items.data.length");
+
+      if (items.data.length === index + 1) {
+        return (
+          <TableRow ref={ref} key={item.id}>
+            <TableCell>{item.lastName}</TableCell>
+            <TableCell>{item.firstName}</TableCell>
+            <TableCell>{item.fatherName}</TableCell>
+            <TableCell>{item.age}</TableCell>
+            <TableCell>{item.gender}</TableCell>
+            <TableCell>{item.email}</TableCell>
+            <TableCell>{item.phoneNumber}</TableCell>
+          </TableRow>
+        );
+      }
       return (
         <TableRow key={item.id}>
           <TableCell>{item.lastName}</TableCell>
@@ -61,7 +76,6 @@ function App() {
     })
   );
 
-  console.log(inView);
   const [modalOpen, setModalOpen] = useState(false);
 
   useEffect(() => {
@@ -71,9 +85,26 @@ function App() {
   }, [inView, hasNextPage, fetchNextPage]);
 
   return status === "pending" ? (
-    <p>Loading...</p>
+    <div className="flex justify-center mt-20">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        className="animate-spin stroke-[#4f99db]"
+      >
+        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+    </div>
   ) : status === "error" ? (
-    <p>Error: {error.message}</p>
+    <div className="flex justify-center mt-20">
+      <p>Error: {error.message}</p>
+    </div>
   ) : (
     <>
       <div className="max-w-[1300px] mx-auto py-20 px-10 border rounded-lg mt-20 shadow-md bg-white">
@@ -98,30 +129,38 @@ function App() {
           </DialogContent>
         </Dialog>
 
-        <ScrollArea className="h-[525px]">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Last Name</TableHead>
-                <TableHead>First Name</TableHead>
-                <TableHead>Father Name</TableHead>
-                <TableHead>Age</TableHead>
-                <TableHead>Gender</TableHead>
-                <TableHead>Email</TableHead>
-                <TableHead>Phone number</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>{dataList}</TableBody>
-          </Table>
-          <Button
-            ref={ref}
-            disabled={isFetchingNextPage || !hasNextPage}
-            onClick={() => fetchNextPage()}
-          >
-            {isFetchingNextPage ? "Loading..." : "Load more"}
-          </Button>
-          {isFetchingNextPage && <p className="text-center">Loading more...</p>}
-        </ScrollArea>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Last Name</TableHead>
+              <TableHead>First Name</TableHead>
+              <TableHead>Father Name</TableHead>
+              <TableHead>Age</TableHead>
+              <TableHead>Gender</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Phone number</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>{dataList}</TableBody>
+        </Table>
+        {isFetchingNextPage && (
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="animate-spin stroke-[#4f99db]"
+            >
+              <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+            </svg>
+          </div>
+        )}
       </div>
     </>
   );
